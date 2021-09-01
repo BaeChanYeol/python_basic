fc30f1f8f558efe27fe3af0313fb4c07d23debe8
ChanYeol <qocksduf94@gmail.com>
2021-09-01 20:31:46 +0900

    python 7일차 커밋

--- python/Day05/selenium_basic.py
+++ python/Day05/selenium_basic.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 import time
 
 # 다운로드 받은 크롬 물리드라이버 가동 명령
-driver = webdriver.Chrome('C:/Users/admin/Desktop/java_web_LKM/python/crawling/chromedriver.exe')
+driver = webdriver.Chrome(r'C:\Users\USER\Desktop\chanyeol\Java_Web_BCY\python\crawling\chromedriver.exe')
 
 # 물리 드라이버로 사이트 이동 명령
 driver.get('https://www.naver.com')
